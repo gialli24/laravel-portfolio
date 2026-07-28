c0b689d9fa81622d0d7fec11f43647b46f5072ca
gialli24 <a.gialli2409@gmail.com>
2026-07-28 18:40:29 +0200

    linked model relationships

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -3,10 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Project extends Model
 {
     public function type() {
         return $this->belongsTo(Type::class);
+    }
+
+    public function technologies() {
+        return $this->BelongsToMany(Technology::class);
     }
 }
